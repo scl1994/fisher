5fcf97a0df3e379333d4b104430dbf9bb3f52168
scl1994 <sunchenglu1994@163.com>
2018-05-24 17:58:01 +0800

    quickstart of flask

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, make_response
 
 app = Flask(__name__)
+app.config.from_object("config")
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    # response = make_response("<html></html>", 200)
+    # response.headers = {"content-type": "text/plain"}
+    # return response
+    return "<html></html>", 200, {"content-type": "text/plain"}
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=app.config["DEBUG"], host="0.0.0.0")
