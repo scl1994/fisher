702d4ecaf078b60a03170b03fd2c5c6193f72e29
scl1994 <sunchenglu1994@163.com>
2018-06-17 18:03:34 +0800

    reorganized configuration files

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("config")
+    app.config.from_object("apps.secure")
+    app.config.from_object("apps.setting")
     register_blueprints(app)
     return app
 
--- apps/web/book.py
+++ apps/web/book.py
@@ -1,20 +1,22 @@
-from flask import jsonify
+from flask import jsonify, request
 
+from apps.forms.book import SearchForm
 from . import web
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
 
 
-@web.route('/book/search/<q>/<page>')
-def search(q, page):
-    """
-    :param q: 普通搜索关键字或者isbn号
-    :param page:
-    :return:
-    """
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == "isbn":
-        result = YuShuBook.search_by_isbn(q)
+@web.route('/book/search')
+def search():
+    form = SearchForm(request.args)
+    if form.validate():
+        q = form.q.data.strip()
+        page = form.page.data
+        isbn_or_key = is_isbn_or_key(q)
+        if isbn_or_key == "isbn":
+            result = YuShuBook.search_by_isbn(q)
+        else:
+            result = YuShuBook.search_by_keyword(q, page)
+        return jsonify(result)
     else:
-        result = YuShuBook.search_by_keyword(q)
-    return jsonify(result)
+        return jsonify(form.errors)
